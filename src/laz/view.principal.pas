d7b25ab555dd5b642913e3c8cda1c9e0f02349e3
Gladiston Santana <gladiston.santana@gmail.com>
2021-12-08 16:11:04 -0300

    Release inicial

---
+++ src/laz/view.principal.pas
@@ -0,0 +1,120 @@
+unit view.principal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, Menus,
+  ActnList, TDIClass;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    actClientes: TAction;
+    actFornecedores: TAction;
+    actRepresentantes: TAction;
+    ActionList1: TActionList;
+    MainMenu1: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    TabSheet1: TTabSheet;
+    TDINoteBook1: TTDINoteBook;
+    procedure actClientesExecute(Sender: TObject);
+    procedure actFornecedoresExecute(Sender: TObject);
+    procedure actRepresentantesExecute(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses view.clientes, view.fornecedores, view.representantes;
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.actClientesExecute(Sender: TObject);
+var
+  i:Integer;
+begin
+  i:=TDINoteBook1.FindFormInPages(fmClientes);
+  if i<0 then
+  begin
+    fmClientes:=TfmClientes.Create(Self);
+    TDINoteBook1.ShowFormInPage(fmClientes, 0);
+  end
+  else
+  begin
+    TDINoteBook1.ShowFormInPage(fmClientes, 0);
+  end;
+end;
+
+procedure TForm1.actFornecedoresExecute(Sender: TObject);
+var
+  i:Integer;
+begin
+  i:=TDINoteBook1.FindFormInPages(fmFornecedores);
+  if i<0 then
+  begin
+    fmFornecedores:=TfmFornecedores.Create(Self);
+    TDINoteBook1.ShowFormInPage(fmFornecedores, 0);
+  end
+  else
+  begin
+    TDINoteBook1.ShowFormInPage(fmFornecedores, 0);
+  end;
+end;
+
+procedure TForm1.actRepresentantesExecute(Sender: TObject);
+var
+  i:Integer;
+begin
+  i:=TDINoteBook1.FindFormInPages(fmRepresentantes);
+  if i<0 then
+  begin
+    fmRepresentantes:=TfmRepresentantes.Create(Self);
+    TDINoteBook1.ShowFormInPage(fmRepresentantes, 0);
+  end
+  else
+  begin
+    TDINoteBook1.ShowFormInPage(fmRepresentantes, 0);
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  // nboHidePageListPopup: Não tem popup para mostrar as tabs(paginas)
+  // nboShowAddTabButton: Não mostra opção para adicionar nova tab
+  // nboShowCloseButtons: Botões de fechar não serão exibidos
+  TDINoteBook1.Options:=[nboHidePageListPopup, nboShowAddTabButton, nboShowCloseButtons];
+  // Não mostra a orelhinha das tabs
+  TDINoteBook1.ShowTabs:=false;
+  // Como se trata de um aplicativo comercial e não um navegador então
+  // é melhor esconder outros botões
+  TDINoteBook1.TDIActions.CloseAllTabs.Visible:=false;
+  TDINoteBook1.TDIActions.CloseTab.Visible:=false;
+  TDINoteBook1.TDIActions.NextTab.Visible:=false;
+  TDINoteBook1.TDIActions.PreviousTab.Visible:=false;
+  TDINoteBook1.TDIActions.TabsMenu.Visible:=false;
+  // deixa ao menos 1 tab para servir de container de outros forms
+  if TDINoteBook1.PageCount<1 then
+    TDINoteBook1.AddTabSheet;
+  // A primeira tab nunca poderá ser fechada
+  // Isso garante que sempre terá uma tab
+  TDINoteBook1.FixedPages:=0;
+end;
+
+end.
+
